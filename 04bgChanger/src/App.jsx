95f40c80d5a5ddf95450082cbf8488e0c2502b9c
roshaans2 <roshaansathya@gmail.com>
2023-12-29 22:01:44 +0530

    bgChanger

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color,setColor] = useState("olive")
+  
+  return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button  className='outline-none px-4 py-1 rounded-full  shadow-lg text-black' onClick={()=>setColor("red")}>Red</button>
+          <button  className='outline-none px-4 py-1 rounded-full  shadow-lg text-black' onClick={()=>setColor("green")}>Green</button>
+          <button  className='outline-none px-4 py-1 rounded-full  shadow-lg text-black' onClick={()=>setColor("blue")}>Blue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
